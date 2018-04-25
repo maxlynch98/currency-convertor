cda96e5e183811e09ce672e1a5a0cb94a9c80775
maxlynch98 <35769339+maxlynch98@users.noreply.github.com>
2018-04-25 13:16:09 +0100

    Add files via upload
    Conversion Project Started

---
+++ js/convert.js
@@ -0,0 +1,10 @@
+function euroToPounds()[
+
+  var amount = parseInt(document.getElementById("value1").value);
+
+  var pounds = amount * 0.86;
+  var message = amount + ' euros converts to ' + pounds + ' sterling.';
+  cansole.log(message);
+  doc.getElementById("resultsentence").innerHTML = message;
+
+]
